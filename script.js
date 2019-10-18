2d068f73b54ee5101bfd3469fdb1fcdb159330a1
suspectpz <bkquach96@gmail.com>
2019-10-17 21:56:12 -0700

    added midterm files

---
+++ script.js
@@ -0,0 +1,20 @@
+var pass1 = document.getElementById('password');
+var pass2 = document.getElementById('confirm_password');
+
+function checkIt() {
+	if (pass1.value.length < 4 || pass2.value.length < 4 || pass1.value.length > 8 || pass2.value.length > 8) {
+  	document.getElementById('message').style.visibility = 'visible';
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'The length of user name and password must be between 4 and 8';
+  }
+  else if (pass1.value == pass2.value) {
+  	document.getElementById('message').style.visibility = 'visible';
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'The input values are correct';
+  }
+  else {
+  	document.getElementById('message').style.visibility = 'visible';
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'The passwords are different';
+  }
+}
