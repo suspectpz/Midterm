7bfc5980cf763fc15706c5b7608e1e851fdc6219
suspectpz <bkquach96@gmail.com>
2019-10-19 19:17:15 -0700

    fixed errors on js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var user = document.getElementById('username');
 var pass1 = document.getElementById('password');
 var pass2 = document.getElementById('confirm_password');
 
 function checkIt() {
-	if (pass1.value.length < 4 || pass2.value.length < 4 || pass1.value.length > 8 || pass2.value.length > 8) {
+
+  if (user.value.length < 4 || pass1.value.length < 4 || pass2.value.length < 4 || user.value.length > 8 || pass1.value.length > 8 || pass2.value.length > 8) {
   	document.getElementById('message').style.visibility = 'visible';
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'The length of user name and password must be between 4 and 8';
@@ -14,7 +16,7 @@ function checkIt() {
   }
   else {
   	document.getElementById('message').style.visibility = 'visible';
-    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'The passwords are different';
   }
 }
